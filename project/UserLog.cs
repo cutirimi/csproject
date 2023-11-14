a4f082e8b023e4173ae8fc6c4ac652722129b665
rimi <132326530+cutirimi@users.noreply.github.com>
2023-11-14 21:31:58 +0900

    fix : UserLog폼 메세지박스 두번 뜸 미수정

--- project/UserLog.cs
+++ project/UserLog.cs
@@ -31,19 +31,27 @@ namespace project
         private void button1_Click(object sender, EventArgs e)
         {
             Confirm cf = new Confirm();
-
+            bool match = true;
             for (int i = 0; i < 2; i++)
             {
                 if (lognametb.Text== add.getName() && logpwtb.Text== add.getPassword())
                 {
-                    MessageBox.Show(add.getName() + "님 환영합니다.");
-                    this.Hide();
-                    cf.Show();
+                    match = true;
                 }
                 else
                 {
-                    MessageBox.Show("아이디 혹은 비밀번호가 일치하지않습니다.\n 다시 입력해주세요.");
+                    match = false;
                 }
+            }
+            if(match==true)
+            {
+                MessageBox.Show(add.getName() + "님 환영합니다.");
+                this.Hide();
+                cf.Show();
+            }
+            else if(match==false)
+            {
+                MessageBox.Show("아이디 혹은 비밀번호가 일치하지않습니다.\n 다시 입력해주세요.");
             }
         }
     }
